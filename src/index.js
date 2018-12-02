444c74bf0fd88802b687a8106d71e2d5dd6a62f2
ugh <wildtangeant@gmail.com>
2018-12-01 18:39:49 -0800

    moved comment time,image,author, and text to to props

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
+
+
 const App= () => {
   return(
     <div className="ui container comments">
-
-      <CommentDetail />
+      <CommentDetail author="george" time="yesterday" text="nothing is certain" img = {faker.image.avatar()} />
+      <CommentDetail author="no" time="tomorrow" text="some things are sauce" img = {faker.image.avatar()} />
+      <CommentDetail author="hellyes" time="rightnow" text="all people lie" img = {faker.image.avatar()} />
     </div>
   );
 };
